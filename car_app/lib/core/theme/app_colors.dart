eccaafe412eaf8a5d8aa782c5a8715d310d60fee
Chuijazy <chuijazy.2004@gmail.com>
2025-01-15 18:28:22 +0600

    ad colors

---
+++ car_app/lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color redButton = Color (0xffC64949);
+  static const Color textColor = Color(0xffF6F6F6);
+}
---
+++ car_app/lib/core/theme/app_textstyles.dart
@@ -0,0 +1,14 @@
+import 'package:car_app/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppTextstyles {
+  static const TextStyle mainText = TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.textColor);
+
+  static const TextStyle buttonText = TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        );
+}
