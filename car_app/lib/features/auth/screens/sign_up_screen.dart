6827f2b2cbb281572d4b43d47d99d5bf883f3300
Chuijazy <chuijazy.2004@gmail.com>
2025-01-17 14:09:45 +0600

    update sign up

--- car_app/lib/features/auth/screens/sign_up_screen.dart
+++ car_app/lib/features/auth/screens/sign_up_screen.dart
@@ -1,177 +1,211 @@
 import 'package:car_app/core/extencions/textstyle_extension.dart';
 import 'package:car_app/core/resource/app_assets.dart';
-//import 'package:car_app/core/resource/app_assets.dart';
 import 'package:car_app/core/theme/app_textstyles.dart';
 import 'package:flutter/material.dart';
+
+import 'sign_in_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    final _formKey = GlobalKey<FormState>(); // Form key for validation
     bool isPasswordVisible = false;
-    return  Scaffold(
+
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(left: 28, right: 13),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20,),
-              Text('Sign Up', 
-              style: AppTextstyles.regular.setSize(48),
-              ),
-              SizedBox(height: 40,),
-              Text('FULL NAME',
-              style: AppTextstyles.regular.setSize(14),
-              ),
-              TextFormField(
-                decoration: 
-                const InputDecoration(
-                  hintText: 'Lorem Ipsum',),
-              ),
-              SizedBox(height: 20,),
-              Text('EMAIL OR PHONE',
-              style: AppTextstyles.regular.setSize(14),
-              ),
-              TextFormField(
-                decoration: 
-                const InputDecoration(
-                  hintText: 'Loremipsum@gmail.com',),
-              ),
-              SizedBox(height: 25,),
-              Text('PASSWORD',
-              style: AppTextstyles.regular.setSize(14),
-              ),
-              StatefulBuilder(
-              builder: (context, setState) {
-             return TextFormField(
-                obscureText: !isPasswordVisible,
-                decoration: InputDecoration(
-                  hintText: '******',
-                  suffixIcon: IconButton(
-                    icon: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off),
-                  onPressed: () {
-                setState(() {
-              isPasswordVisible = !isPasswordVisible;
-               });
-               },
-              ),
-              ),
-             );
-           },
-           ),
-              SizedBox(height: 20,),
-              ElevatedButton( 
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  padding: EdgeInsets.zero
+          padding: const EdgeInsets.symmetric(horizontal: 28),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  'Sign Up',
+                  style: AppTextstyles.regular.setSize(48),
                 ),
-                onPressed: (){},
-                child: Container(
-                  width: 350,
-                  height: 52,
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 43,),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.transparent, width: 2,
-                    ),
-                    color: Color(0xff2B4C59),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                  Text('Log In',
-                  style: AppTextstyles.semiBold.setSize(20).copyWith(color: Colors.white),
-                  ),
-                 ], ),
+                const SizedBox(height: 40),
+                Text('FULL NAME', style: AppTextstyles.regular.setSize(14)),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Lorem Ipsum'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              Center(child: Text('OR', 
-              style: TextStyle(fontSize: 14),),),
-              SizedBox(height: 10,),
-              // ignore: avoid_unnecessary_containers
-              ElevatedButton( 
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  padding: EdgeInsets.zero
+                const SizedBox(height: 20),
+                Text('EMAIL', style: AppTextstyles.regular.setSize(14)),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Loremipsum@gmail.com'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Please enter a right email';
+                    }
+                    return null;
+                  },
                 ),
-                onPressed: (){},
-                child: Container(
-                  width: 350,
-                  height: 52,
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 43,),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.black, width: 2,
-                    )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset(AppAssets.google, width: 30, height: 30,),
-                  Text('Continue With Google',
-                  ),
-                 ], ),
+                const SizedBox(height: 25),
+                Text('PASSWORD', style: AppTextstyles.regular.setSize(14)),
+                StatefulBuilder(
+                  builder: (context, setState) {
+                    return TextFormField(
+                      obscureText: !isPasswordVisible,
+                      decoration: InputDecoration(
+                        hintText: '******',
+                        suffixIcon: IconButton(
+                          icon: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              isPasswordVisible = !isPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        } else if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                    );
+                  },
                 ),
-              ),
-              SizedBox(height: 5,),
-             ElevatedButton( 
-              style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  padding: EdgeInsets.zero,),
-              onPressed: (){},
-                child: Container(
-                  width: 350,
-                  height: 52,
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 43,),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.black, width: 2,
-                    )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset(AppAssets.facebook, width: 25, height: 25,),
-                  Text('Continue With Facebook',
-                  ),
-                 ], ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Don’t Have an account yet?',
-                  style: TextStyle(fontSize: 10),),
-                  SizedBox(width: 100,),
-                  ElevatedButton(onPressed: (){}, 
+                const SizedBox(height: 20),
+                ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  padding: EdgeInsets.zero,),
-                  child: Text('SIGN IN', 
-                  style: TextStyle(fontSize: 10).copyWith(color: Color(0xffFCC21B,
-),),
-                  ),),
-                ],
-              ),
-              
-        ]
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // ignore: avoid_print
+                      print('');
+                    } else {
+                      // ignore: avoid_print
+                      print('Error');
+                    }
+                  },
+                  child: Container(
+                    width: 350,
+                    height: 52,
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 43),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.transparent, width: 2),
+                      color: const Color(0xff2B4C59),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: AppTextstyles.semiBold.setSize(20).copyWith(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'OR',
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: () {},
+                  child: Container(
+                    width: 350,
+                    height: 52,
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 43),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(AppAssets.google, width: 30, height: 30),
+                        const Text('Continue With Google'),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: () {},
+                  child: Container(
+                    width: 350,
+                    height: 52,
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 43),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(AppAssets.facebook, width: 25, height: 25),
+                        const Text('Continue With Facebook'),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don’t Have an account yet?',
+                      style: TextStyle(fontSize: 10),
+                    ),
+                    const SizedBox(width: 100),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignInScreen(),),);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: const Text(
+                        'SIGN IN',
+                        style: TextStyle(fontSize: 10, color: Color(0xffFCC21B)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-        ),
-       ),
-      );
+      ),
+    );
   }
 }
